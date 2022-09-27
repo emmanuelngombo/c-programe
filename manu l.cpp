2fb4b0090c9cda2f0fd13ee790e64e5d9740f254
emmanuelngombo <114079873+emmanuelngombo@users.noreply.github.com>
2022-09-27 10:27:09 +0300

    Add files via upload
    largest number

---
+++ manu l.cpp
@@ -0,0 +1,32 @@
+//program for the lagest number
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int num1,num2,num3;
+
+cout<<"enter the three numbers";
+
+cin>>num1>>num2>>num3;
+
+if(num1>num2&num1>num3){
+  
+
+cout<<"num1 is the largest";
+}
+
+else if(num2>num1&num2>num3){
+
+cout<<"num2 is the largest";
+}
+
+else if(num3>num1&num3>num2){
+
+cout<<"num3 is the largest";
+
+}   
+
+return 0;
+
+}
